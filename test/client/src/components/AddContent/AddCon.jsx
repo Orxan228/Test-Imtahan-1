2ae9a19009c6b60dc494a73281df218bdb03d675
Orxan228 <orxan.qasimov.2003@gmail.com>
2023-05-20 18:22:47 +0400

    Frontend

--- test/client/src/components/AddContent/AddCon.jsx
+++ test/client/src/components/AddContent/AddCon.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React from "react";
+import "./AddCon.scss";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import {useFormik} from "formik"
+import * as Yup from "yup"
+
+const validationSchema = Yup.object({
+  name: Yup
+  .string("Enter a Brand Name")
+  .required("First Name is required"),
+  price: Yup
+  .string("Enter a price")
+  .required("Price is required"),
+});
 
 const AddCon = () => {
+
+  const formik = useFormik({
+    initialValues:{
+      name:"",
+      price:"",
+    },
+    validationSchema:validationSchema,
+    onSubmit:(values) =>{
+      alert(JSON.stringify(values, null,2));
+      values.name=""
+      values.price=""
+    },
+  })
+
   return (
     <>
-    <div className="addcon">
-      
-    </div>
+      <div className="addcon">
+        <div className="addcon___form">
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+              label="Brand Name"
+              variant="outlined"
+            />
+            <TextField 
+              fullWidth
+              id="price"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+              label="Price" 
+              variant="outlined" 
+            />
+            <Button 
+              fullWidth
+              type="submit"
+              variant="contained">
+              Add
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddCon
+export default AddCon;
